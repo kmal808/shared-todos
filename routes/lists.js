82f47db9817ad0b6aeef9f37b3fcc88cb939a7e2
MaVi <90439054+MaViSunshine@users.noreply.github.com>
2022-09-01 21:44:03 -0300

    Update lists.js

--- routes/lists.js
+++ routes/lists.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const listsController = require('../controllers/lists') 
 const { ensureAuth } = require('../middleware/auth')
 
-router.get('/lists', ensureAuth, listsController.getLists)
+router.get('/', ensureAuth, listsController.getLists)
 
-router.post('/lists/createList', listsController.createList)
+router.post('/createList', listsController.createList)
 
-router.put('/lists/markComplete', listsController.markComplete)
+router.put('/markComplete', listsController.markComplete)
 
-router.put('/lists/markIncomplete', listsController.markIncomplete)
+router.put('/markIncomplete', listsController.markIncomplete)
 
-router.delete('/lists/deleteList', listsController.deleteList)
+router.delete('/deleteList', listsController.deleteList)
 
 module.exports = router
