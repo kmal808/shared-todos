8531fdce49e7633cb69217b1074b0c1c9de83a39
MaViSunshine <victoriaserty@gmail.com>
2022-09-04 23:07:26 -0300

    style changes

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,4 +1,5 @@
 document.getElementById('toggle').addEventListener('click', () => {
 	document.getElementById('blender').classList.toggle('expand')
 	document.getElementById('toggle').classList.toggle('hasZIndex')
+
 })
